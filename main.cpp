1fe4ff3d9f8072b4d370affd040ef25f1dc55ba8
chhavi17 <chhavi17mehta@gmail.com>
2018-04-14 04:49:48 -0700

    added a schedular task for 3D to 2D transformation of axis and cube

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <TFT_22_ILI9225.h>
 #include "tasks.hpp"
+#include "ssp0.h"
+#include "printf_lib.h"
 #include "utilities.h"
 #include "io.hpp"
 
@@ -14,68 +16,57 @@
 #define OFFSETX 176/2                  // offsets
 #define OFFSETY 176/2
 #define OFFSETZ 30
-#define magic 176/2
-const signed int a[4] = { 0, 20, 0, 0 };
-const signed int b[4] = { 0, 0, -20, 0 };
-const signed int c[4] = { 0, 0, 0, 20 };
-const int f[3] = { 1, 1, 1 };          // start vertex for lines
-const int g[3] = { 2, 3, 4 };          // end vertex for lines
+#define f 176/2
 
-int sx[5], sy[5], ex[5], ey[5];                    // define global vars for calling graphics subroutines
-uint32_t color_[3] = { COLOR_RED, COLOR_BLUE, COLOR_GREEN };
-
-
-class LCD: public scheduler_task
-{
+class LCD: public scheduler_task {
 private:
-        TFT_22_ILI9225 lcd;
-        bool flag = true;
-        int x,y,x1,y1,x2,y2,x3,y3;
-        int side;
-public:
-    LCD(uint8_t priority) : scheduler_task("LCD_interface", 1024*20, priority){    }
 
-    bool init(){
-        lcd.setValues(RST,RS,CS,BL);
+    TFT_22_ILI9225 lcd;
+    bool flag = true;
+    int x, y, x1, y1, x2, y2, x3, y3;
+    int side;
+
+public:
+    LCD(uint8_t priority) :
+            scheduler_task("LCD_interface", 1024 * 20, priority)
+    {
+    }
+
+    bool init()
+    {
+        lcd.setValues(RST, RS, CS, BL);
         return true;
     }
 
-    bool run(void *p){
-        if (flag)
-        {
+    bool run(void *p)
+    {
+        if (flag) {
             lcd.begin();
             flag = false;
         }
 
         //-----natural scene of beach-----//
         /*lcd.drawLine(88,25,92,27,COLOR_BLACK);
-        lcd.drawLine(92,27,90,29,COLOR_BLACK);
-        lcd.drawLine(89,25,93,27,COLOR_BLACK);
-        lcd.drawLine(93,27,91,29,COLOR_BLACK);
+         lcd.drawLine(92,27,90,29,COLOR_BLACK);
+         lcd.drawLine(89,25,93,27,COLOR_BLACK);
+         lcd.drawLine(93,27,91,29,COLOR_BLACK);
 
-        //lcd.drawTriangle(0,0,50,100,150,100,COLOR_WHITE);
-        lcd.fillCircle(25,25,15,COLOR_GOLD);
-        lcd.fillRectangle(140,219,175,0,COLOR_SKYBLUE);*/
+         //lcd.drawTriangle(0,0,50,100,150,100,COLOR_WHITE);
+         lcd.fillCircle(25,25,15,COLOR_GOLD);
+         lcd.fillRectangle(140,219,175,0,COLOR_SKYBLUE);*/
 
         //----- ScreenSaver-1 -----//
         //lcd.screenSS(0);
 
-
-        //trees
-        //x = LCD_WIDTH/2;
-        x = rand()%LCD_WIDTH;
+        //-------working code for trees--------//
+        x = rand() % LCD_WIDTH;
         y = 0;
-        uint32_t color = rand()%0xFFFFFFu;
+        uint32_t color = rand() % 0xFFFFFFu;
 
-            lcd.draw_tree(x,y,color);
-
+        lcd.draw_tree(x, y, color);
 //lcd.drawLine(LCD_WIDTH/2,0,LCD_WIDTH+10,LCD_HEIGHT-15,color);
 
-
-
-
-
-//        //working code for squares
+        //-------------working code for squares-------------//
 //        x = rand()% LCD_WIDTH;
 //        y = rand()% LCD_HEIGHT;
 //        side = rand()% LCD_WIDTH;
@@ -97,119 +88,7 @@ public:
 //            lcd.startSS1(x,y,x1,y1,x2,y2,x3,y3,color);
 //        }
 
-
-      int newx[4]; //projected 2d X
-        int newy[4]; //projected 2d Y
-        int i, loop;
-        float xt, yt, zt, x, y, z, sinax, cosax, sinay, cosay, sinaz, cosaz, vertex;
-        float xpos = 0; // position for object in 3d space, in x
-        float ypos = 0; // y
-        float zpos = 0; // and z values
-        float rotx = 0; // starting amount of x rotation
-        float roty = 0; // starting amount of y rotation
-        float rotz = 0; // starting amount of z rotation
-        bool change = false;
-
-     //   for (loop = 0; loop <= 100; loop++) // rotate the axis 100 times
-        while(1)
-
-                {
-
-            //translation
-            xpos = xpos + 0.0;            // move the object
-            ypos = ypos + 0.0;
-            zpos = zpos + 0.0;
-
-            //rotations by 10 degrees
-            if (SW.getSwitch(1))
-            {
-                rotx = rotx + (10 * (3.14 / 180));
-                change = true;
-
-            }
-            if (SW.getSwitch(2))
-            {
-                roty = roty + (10*(3.14/180));
-                change = true;
-                // rotate on Y axis
-            }
-            if (SW.getSwitch(3))
-            {
-                rotz = rotz + (10*(3.14/180));                // rotate on Z axis
-                change = true;
-            }
-
-            //can be made to lookup table
-            sinax = sin(rotx);
-            cosax = cos(rotx);
-
-            sinay = sin(roty);
-            cosay = cos(roty);
-
-            sinaz = sin(rotz);
-            cosaz = cos(rotz);
-
-            for (i = 0; i < 4; i++)            // translate 3d vertex position to 2d screen position
-                    {
-                x = a[i];                    // get x for vertex i
-                y = b[i];                    // get y for vertex i
-                z = c[i];                    // get z for vertex i
-
-                yt = y * cosax - z * sinax;                    // rotate around the x axis
-                zt = y * sinax + z * cosax;                    // using the Y and Z for the rotation
-                y = yt;
-                z = zt;
-
-                xt = x * cosay - z * sinay;                    // rotate around the Y axis
-                zt = x * sinay + z * cosay;                    // using X and Z
-                x = xt;
-                z = zt;
-
-                xt = x * cosaz - y * sinaz;                    // finaly rotate around the Z axis
-                yt = x * sinaz + y * cosaz;                    // using X and Y
-                x = xt;
-                y = yt;
-
-                x = x + xpos;                    // add the object position offset
-                y = y + ypos;                    // for both x and y
-                z = z + OFFSETZ - zpos;         // as well as Z
-
-                newx[i] = (x * magic / z) + OFFSETX;                    // translate 3d to 2d coordinates for screen
-                newy[i] = (y * magic / z) + OFFSETY;                    // drawing
-            }
-
-          //  lcd.clear();                    // clear the screen to remove old axis
-        if(change == true)
-        {
-            lcd.drawLine(sx[0], sy[0], ex[0], ey[0], COLOR_BLACK);
-           lcd.drawLine(sx[1], sy[1], ex[1], ey[1], COLOR_BLACK);
-           lcd.drawLine(sx[2], sy[2], ex[2], ey[2], COLOR_BLACK);
-            change = false;
-        }
-
-
-            for (i = 0; i < 3; i++)                    // draw the lines that make up the object
-{
-                char c;
-                vertex = f[i] - 1;         // temp = start vertex for this line
-                sx[i] = newx[(int) vertex];         // set line start x to vertex[i] x position
-                sy[i] = newy[(int) vertex];         // set line start y to vertex[i] y position
-                vertex = g[i] - 1;         // temp = end vertex for this line
-                ex[i] = newx[(int) vertex];         // set line end x to vertex[i+1] x position
-                ey[i] = newy[(int) vertex];         // set line end y to vertex[i+1] y position
-                lcd.drawLine(sx[i], sy[i], ex[i], ey[i], color_[i]);
-
-
-//                  if (i == 0) lcd.drawChar(10, 10, 'X', color_[i]);
-//                else if (i == 1) lcd.drawChar(10, 20, 'Y', color_[i]);
-//                else
-//                    lcd.drawChar(10, 30, 'Z', color_[i]);
-
-            }
-        }
-
-
-        if (SW.getSwitch(1))
+        if (SW.getSwitch(4))
         {
             lcd.clear();
         }
@@ -218,11 +97,185 @@ public:
     }
 };
 
+class Transformation: public scheduler_task {
+private:
+
+    TFT_22_ILI9225 lcd;
+    bool flag;
+
+    //Lookup table for sin and cosine values
+    float sin[18] = { 0, 0.34, 0.64, 0.87, 0.98, 0.98, 0.87, 0.64, 0.34, 0, -0.34, -0.64, -0.87, -0.98, -0.98, -0.87, -0.64, -0.34 };
+    float cos[18] = { 1, 0.94, 0.77, 0.5, 0.17, -0.17, -0.5, -0.77, -0.94, -1, -0.94, -0.77, -0.5, -0.17, 0.17, 0.5, 0.77, 0.94 };
+
+    float a_px, a_py, a_pz;                     //after rotation temp values
+    int rotx = 0, roty = 0, rotz = 0;           //rotation angle iterators for x, y, z-axis
+    int newx[10] = { 0 }, newy[10] = { 0 };     //2D co-ordinates
+    bool change;
+
+    //color for axes
+    uint32_t color_[3] = { COLOR_RED, COLOR_BLUE, COLOR_GREEN };
+
+    //Cube
+    int sv[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4 };          // start vertex for lines
+    int ev[12] = { 2, 3, 4, 1, 6, 7, 8, 5, 6, 7, 8, 5 };          // end vertex for lines
+
+public:
+    Transformation(uint8_t priority) :
+            scheduler_task("Rotating 3D cube and displaying in 2D plane", 1024 * 20, priority)
+    {
+    }
+
+    bool init()
+    {
+        lcd.setValues(RST, RS, CS, BL);
+        return true;
+    }
+
+    bool run(void *p)
+    {
+        if (flag) {
+            lcd.begin();
+            flag = false;
+        }
+
+
+        //Original Points for axis
+        //        int b_px[10] = { 0, 20, 0, 0 };
+        //        int b_py[10] = { 0, 0, -20, 0 };
+        //        int b_pz[10] = { 0, 0, 0, 20 };
+
+        //Original Points for cube
+        int b_px[10] = { 0, 10, 10, 0, 0, 0, 10, 10 };
+        int b_py[10] = { 0, 0, 0, 0, 10, 10, 10, 10 };
+        int b_pz[10] = { 0, 0, 10, 10, 10, 0, 0, 10 };
+
+        if (SW.getSwitch(1)) //rotation wrt x-axis
+        {
+            if(rotx == 17) rotx = 0;
+            else
+            rotx += 1;
+            change = true;
+        }
+        if (SW.getSwitch(2)) //rotation wrt y-axis
+        {
+
+            if(roty == 17) roty = 0;
+            else
+            roty += 1;
+            change = true;
+        }
+        if (SW.getSwitch(3)) //rotation wrt z-axis
+        {
+            if(rotz == 17) rotz = 0;
+            else
+            rotz += 1;
+            change = true;
+        }
+
+        //Rotation matrix
+        for (int i = 0; i < 8; i++) {
+            a_py = b_py[i] * cos[rotx] - b_pz[i] * sin[rotx];
+            a_pz = b_py[i] * sin[rotx] + b_pz[i] * cos[rotx];
+            b_py[i] = a_py;
+            b_pz[i] = a_pz;
+
+            a_px = b_px[i] * cos[roty] + b_pz[i] * sin[roty];
+            a_pz = -(b_px[i] * sin[roty]) + b_pz[i] * cos[roty];
+            b_px[i] = a_px;
+            b_pz[i] = a_pz;
+
+            a_px = b_px[i] * cos[rotz] - b_py[i] * sin[rotz];
+            a_py = b_px[i] * sin[rotz] + b_py[i] * cos[rotz];
+            b_py[i] = a_py;
+            b_px[i] = a_px;
+
+            if (change == true) {
+                //for axis
+                //                      for (int j = 1; j < 4; j++) {
+                //                          lcd.drawLine(newx[0], newy[0], newx[j], newy[j], COLOR_BLACK);
+                //                      }
+                //for cube
+                for (int j = 0; j < 12; j++) {
+                    lcd.drawLine(newx[sv[j] - 1], newy[sv[j] - 1], newx[ev[j] - 1], newy[ev[j] - 1], COLOR_BLACK);
+                }
+                change = false;
+            }
+
+            //scaling and adding offset for 2D co-ordinates
+            b_pz[i] += OFFSETZ;
+            newx[i] = (b_px[i] * f / b_pz[i]) + OFFSETX;
+            newy[i] = (b_py[i] * f / b_pz[i]) + OFFSETY;
+        }
+
+        ////for axis
+        //            for (int j = 1; j < 4; j++) {
+        //                lcd.drawLine(newx[0], newy[0], newx[j], newy[j], color_[j - 1]);
+        //
+        //            }
+
+        //for cube
+        for (int j = 0; j < 12; j++) {
+            lcd.drawLine(newx[sv[j] - 1], newy[sv[j] - 1], newx[ev[j] - 1], newy[ev[j] - 1], COLOR_GOLD);
+        }
+
+        if (SW.getSwitch(4))
+        {
+            lcd.clear();
+        }
+
+        return true;
+
+    }
+};
+
+
+class master: public scheduler_task {
+private:
+    char dat;
+public:
+    master(uint8_t priority) :
+            scheduler_task("SPI_interface", 1024, priority)
+    {
+    }
+
+    bool init()
+    {
+        ssp0_init(1);
+        LPC_PINCON->PINSEL3 |= (3 << 8) | (3 << 14) | (3 << 16);
+
+        LPC_PINCON->PINSEL3 &= ~(3 << 12);
+        LPC_GPIO1->FIODIR |= (1 << 22);
+        LE.on(1);
+
+        ssp0_set_max_clock(4);
+        return true;
+    }
+
+    bool run(void *p)
+    {
+
+        LPC_GPIO1->FIOCLR = (1 << 22);
+
+        dat = ssp0_exchange_byte('C');
+        u0_dbg_printf("received: %c\n", dat);
+        // dat = ssp0_exchange_byte(dat);
+        LE.off(1);
+        LPC_GPIO1->FIOSET = (1 << 22);
+        vTaskDelay(100);
+        return true;
+    }
+};
+
 int main(void)
 {
     //scheduler_add_task(new terminalTask(PRIORITY_HIGH));
 
-    scheduler_add_task(new LCD(2));
+    //----LAB1----
+    //scheduler_add_task(new LCD(2));
+
+    //----LAB2----
+    scheduler_add_task(new Transformation(2));
+    // scheduler_add_task(new master(2));
 
     // Alias to vScheduleRSTart();
     scheduler_start();
